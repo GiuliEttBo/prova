c47fbd26030343f59780cc3315ae23612f40999c
Giulia <g.mischitelli@conterra.de>
2018-05-11 14:02:21 +0200

    fix Triggering Search and bind functions

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
   constructor(props){
     super(props);
-    this.handleTermChange.bind(this);
-    this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
   }
   handleTermChange(event){
-    debugger;
     if(!event){
-      return;
+      //do nothing
     } else {
         this.search(event.target.value);
     }
@@ -24,7 +22,7 @@ class SearchBar extends React.Component{
     return (
       <div className="SearchBar">
         <input placeholder="Enter A Song, Album, or Artist" onSearch={this.handleTermChange}/>
-        <a>SEARCH</a>
+        <a onClick={this.search}>SEARCH</a>
       </div>
 
     );
